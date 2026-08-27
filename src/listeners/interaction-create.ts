4b38d4d086fa50edf7cc1b770b2a354c2bf85e46
yorunoken <me@yorunoken.com>
2026-08-28 01:04:03 +0300

    Contain interaction listener failures

--- src/listeners/interaction-create.ts
+++ src/listeners/interaction-create.ts
@@ -1,11 +1,16 @@
 import { dispatchApplicationCommand } from "commands/dispatch/application-dispatcher";
 import { handlePaginationInteraction } from "../interactions/pagination-handler";
 import { $listener } from "@utils/lilybird-handler";
+import { logger } from "@utils/logger";
 
 $listener({
     event: "interactionCreate",
     handle: async (interaction) => {
-        if (await handlePaginationInteraction(interaction)) return;
-        await dispatchApplicationCommand(interaction);
+        try {
+            if (await handlePaginationInteraction(interaction)) return;
+            await dispatchApplicationCommand(interaction);
+        } catch (error) {
+            await logger.error("Interaction listener failed", error as Error);
+        }
     },
 });
