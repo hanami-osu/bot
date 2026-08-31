0c416b3e36061274fdb0dab5a1f350850c52b46a
yorunoken <me@yorunoken.com>
2026-09-01 01:19:02 +0300

    get rid of prettier and use eslint for formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,33 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
-export default tseslint.config({
-    files: ["**/*.ts", "**/*.js"],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic],
-    rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
-            {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-                caughtErrorsIgnorePattern: "^_",
-            },
-        ],
-        "@typescript-eslint/array-type": ["error", { default: "generic" }],
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: "double",
+        semi: true,
+        jsx: true,
+        braceStyle: "1tbs",
+    }),
+
+    {
+        files: ["**/*.ts", "**/*.js"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+            "@typescript-eslint/array-type": ["error", { default: "generic" }],
+        },
     },
-});
+);
